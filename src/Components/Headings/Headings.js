3725586d14de403a4fdfab0e3ac111f834c21301
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2022-12-03 16:37:31 +0530

    deep_thought implemented

--- src/Components/Headings/Headings.js
+++ src/Components/Headings/Headings.js
@@ -3,9 +3,9 @@ import "./Headings.css";
 import {data} from "../Common/Data";
 
 export default function Headings() {
-console.log(data)
+    console.log(data)
     return <div className={"heading"}>
 
-        {data.tasks[0].assets[3].display_asset_docs}
+dssad
     </div>
 }
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,44 +1,45 @@
 import "./Sidebar.css";
 
 import {useState} from "react";
-import {GrMenu} from "react-icons/gr";
-import {motion} from "framer-motion";
 import {data} from "../Common/Data";
+import {VscThreeBars} from "react-icons/vsc";
 
 export default function Sidebar() {
-    const [selected, setSelected] = useState(0)
     const [open, setOpen] = useState(true)
 
-    const sidebarVariants = {
-        true: {
-            left: '0'
-        },
-        false: {
-            left: '-60%'
-        }
+    const handleClick = () => {
+        setOpen(!open)
     }
-    return <>
-        <div className={"bar-icon"} style={open ? {left: "25%", position: ""} : {left: "0"}}
-             onClick={() => setOpen(!open)}>
-            <GrMenu color={"white"}/>
+    return <div className={"sidebar"}
+                style={open ? {width: "96px"} : {width: "200px"}}
+    >
+        <div className={"bar-icon"}
+
+        >
+            <div style={open ? {display: "none"} : {display: "block"}}>
+                <h6>Journey Board</h6>
+            </div>
+            <VscThreeBars onClick={handleClick}/>
+        </div>
+        <div style={open ? {display: "none"} : {display: "block"}}
+             className={"sidebar-heading"}>
+
+<h4>1. Demo task</h4>
+            <ul>
+                <li className={"menu-item"}>{data.tasks[0].assets[0].asset_title}</li>
+                <li className={"menu-item"}>{data.tasks[0].assets[1].asset_title}</li>
+                <li className={"menu-item"}>{data.tasks[0].assets[2].asset_title}</li>
+                <li className={"menu-item"}>{data.tasks[0].assets[3].asset_title}</li>
+                <li className={"menu-item"}>{data.tasks[0].assets[4].asset_title}</li>
+                <li className={"menu-item"}>{data.tasks[0].assets[5].asset_title}</li>
+                <li className={"menu-item"}>{data.tasks[0].assets[6].asset_title}</li>
+                <li className={"menu-item"}>{data.tasks[0].assets[7].asset_title}</li>
+                <li className={"menu-item"}>{data.tasks[0].assets[8].asset_title}</li>
+            </ul>
+
+
         </div>
 
-        <motion.div
-            variants={sidebarVariants}
-            animate={window.innerWidth <= 1968 ? `${open}` : ''}
-            className={"sidebar"}>
 
-
-            <div className={"menu"}>
-
-
-                <div>
-                    <li className={"menu-item"}>{data.tasks[0].assets.asset_title}</li>
-
-                </div>
-
-
-            </div>
-        </motion.div>
-    </>
+    </div>
 }
